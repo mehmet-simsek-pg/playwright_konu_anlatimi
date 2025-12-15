b9143c596c3bca50267e439d0ac0159fd2585164
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-12-15 17:28:42 +0100

    initial commit

---
+++ src/test/java/Example.java
@@ -0,0 +1,28 @@
+import com.microsoft.playwright.*;
+
+public class Example {
+    public static void main(String[] args) {
+
+        // Playwright baslatildi
+        try (Playwright playwright = Playwright.create()){
+
+            // Browser baslatiliyor
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions()
+                            .setHeadless(false)
+                            .setSlowMo(500)
+            );
+
+            // Yeni tab olusturmak, yani yeni sekme acilmasi icin kullanilir. Zorunlu degildir
+            BrowserContext browserContext = browser.newContext();
+            // Page page = browser.newPage(); bu sekilde de kullanilabilir
+            Page page = browserContext.newPage();
+
+            // burada siteyi actik
+            page.navigate("https://www.saucedemo.com/");
+
+            // browser i kapattik
+            browser.close();
+        }
+    }
+}
