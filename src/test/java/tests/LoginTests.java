b139c1180f71684bfe438539318ae2b2910cf868
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-12-16 18:50:09 +0100

    LoginTests created

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,6 +1,32 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.ProductsPage;
 
 public class LoginTests extends BaseTest {
+
+    @Test
+    public void successfullyLogin() {
+        LoginPage loginPage = new LoginPage(page);
+        ProductsPage productsPage =
+        loginPage.loginAs("standard_user", "secret_sauce");
+
+        // Login sonrasi product page acildi mi?
+        Assert.assertTrue(productsPage.isOpenPage());
+    }
+
+    @Test
+    public void failedLogin() {
+        LoginPage loginPage = new LoginPage(page);
+
+        loginPage.fillUsername("error_user")
+                 .fillPassword("secret_sauce")
+                 .clickLoginBtnFail();
+
+        Assert.assertTrue(loginPage.isErrorMessageVisible());
+
+    }
 }
