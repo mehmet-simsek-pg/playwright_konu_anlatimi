c2d8d1537ab4e7b11b3ec95d900b01aab0fc7429
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-12-16 19:22:26 +0100

    CartTests created

---
+++ src/test/java/tests/CartTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+public class CartTests extends BaseTest {
+
+    @Test
+    public void removeProductFromCart() {
+
+        ProductsPage productsPage = new LoginPage(page)
+                .loginAs("standard_user", "secret_sauce");
+
+        Assert.assertTrue(productsPage.isOpenPage());
+
+        // burada kac eleman eklediysek cart icerisine, o sayida eleman silebiliriz.
+        productsPage.addProductToCart(2);
+        //productsPage.addProductToCart(3); eger bir eleman daha eklersem expected degerim 1 olmali
+        CartPage cartPage = productsPage.clickCartIcon();
+
+        Assert.assertTrue(cartPage.isOpenPage());
+
+        cartPage.removeItem(1);
+
+        Assert.assertEquals(cartPage.getCartItemCount(), 0);
+    }
+}
